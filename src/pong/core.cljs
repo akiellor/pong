bcfe8a8724078d353b3e487119f59326582bd767
Andrew Kiellor <akiellor@gmail.com>
2015-01-24 16:17:48 -0800

    Move rendering out into render ns.

--- src/pong/core.cljs
+++ src/pong/core.cljs
@@ -2,7 +2,8 @@
   (:require
     [figwheel.client :as fw]
     [goog.events :as events]
-    [pong.physics :as p]))
+    [pong.physics :as p]
+    [pong.render :as r]))
 
 (enable-console-print!)
 
@@ -101,57 +102,6 @@
                          :direction [0.01 0.01]
                          :velocity 0.2}}))
 
-(defn fill-style [canvas style]
-  (set! (. canvas -fillStyle) style))
-
-(defn fill-rect [canvas x y dx dy]
-  (.fillRect canvas x y dx dy))
-
-(defn fill-text [canvas text x y]
-  (set! (. canvas -font) "48px Monospace")
-  (set! (. canvas -textAlign) "center")
-  (.fillText canvas text x y))
-
-(defn percentage->coord [percentage height width]
-  (let [[px py] percentage]
-    [(* width px) (* height py)]))
-
-(defn render-rect [canvas rect position]
-  (let [cheight (.-height canvas)
-        cwidth (.-width canvas)
-        height (* cheight (:height rect))
-        width (* cwidth (:width rect))
-        [x y] (percentage->coord position cheight cwidth)]
-    (fill-style canvas "white")
-    (fill-rect canvas x y width height)))
-
-(defn rect? [value] (and (contains? value :rect) (contains? value :position)))
-
-(defn render-rects [canvas state]
-  (let [rects (filter rect? (vals state))]
-    (doall (map #(render-rect canvas (:rect %) (:position %)) rects))))
-
-(defn text? [value] (and (contains? value :text) (contains? value :position)))
-
-(defn render-text [canvas text position]
-  (let [cheight (.-height canvas)
-        cwidth (.-width canvas)
-        [x y] (percentage->coord position cheight cwidth)]
-    (fill-style canvas "white")
-    (fill-text canvas text x y)))
-
-(defn render-texts [canvas state]
-  (let [texts (filter text? (vals state))]
-    (doall (map #(render-text canvas ((:text %) state) (:position %)) texts))))
-
-(defn render [canvas state]
-  (let [height (.-height canvas)
-        width (.-width canvas)]
-    (fill-style canvas "black")
-    (fill-rect canvas 0 0 width height)
-    (render-rects canvas state)
-    (render-texts canvas state)))
-
 (defn physics [state]
   (-> state
       p/movement
@@ -162,7 +112,7 @@
 
 (defn loop-render! [canvas]
   (.requestAnimationFrame js/window (fn [] (loop-render! canvas)))
-  (render canvas @state))
+  (r/render canvas @state))
 
 (defn new-player-position [state player code keycodes]
   (let [current-position (get-in state [player :position 1])
---
+++ src/pong/render.cljs
@@ -0,0 +1,53 @@
+(ns pong.render)
+
+(defn fill-style [canvas style]
+  (set! (. canvas -fillStyle) style))
+
+(defn fill-rect [canvas x y dx dy]
+  (.fillRect canvas x y dx dy))
+
+(defn fill-text [canvas text x y]
+  (set! (. canvas -font) "48px Monospace")
+  (set! (. canvas -textAlign) "center")
+  (.fillText canvas text x y))
+
+(defn percentage->coord [percentage height width]
+  (let [[px py] percentage]
+    [(* width px) (* height py)]))
+
+(defn render-rect [canvas rect position]
+  (let [cheight (.-height canvas)
+        cwidth (.-width canvas)
+        height (* cheight (:height rect))
+        width (* cwidth (:width rect))
+        [x y] (percentage->coord position cheight cwidth)]
+    (fill-style canvas "white")
+    (fill-rect canvas x y width height)))
+
+(defn rect? [value] (and (contains? value :rect) (contains? value :position)))
+
+(defn render-rects [canvas state]
+  (let [rects (filter rect? (vals state))]
+    (doall (map #(render-rect canvas (:rect %) (:position %)) rects))))
+
+(defn text? [value] (and (contains? value :text) (contains? value :position)))
+
+(defn render-text [canvas text position]
+  (let [cheight (.-height canvas)
+        cwidth (.-width canvas)
+        [x y] (percentage->coord position cheight cwidth)]
+    (fill-style canvas "white")
+    (fill-text canvas text x y)))
+
+(defn render-texts [canvas state]
+  (let [texts (filter text? (vals state))]
+    (doall (map #(render-text canvas ((:text %) state) (:position %)) texts))))
+
+(defn render [canvas state]
+  (let [height (.-height canvas)
+        width (.-width canvas)]
+    (fill-style canvas "black")
+    (fill-rect canvas 0 0 width height)
+    (render-rects canvas state)
+    (render-texts canvas state)))
+
